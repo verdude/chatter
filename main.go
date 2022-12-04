c56990d717992aca82c9ac7b6929d696a35855ab
Santiago Verdu <erra@qmake.org>
2022-12-03 22:43:16 -0600

    something sort of

--- main.go
+++ main.go
@@ -1,12 +1,93 @@
 package main
 
 import (
+  "bytes"
+  "flag"
+  "fmt"
+  "io"
+  "net/http"
+  "encoding/json"
+
   "github.com/verdude/zapr"
+  "go.uber.org/zap"
 )
 
+type UserMessage struct {
+  Sender string `json:"sender"`
+  Message string `json:"message"`
+}
+
+type BotMessage struct {
+  RecipientID string `json:"recipient_id"`
+  Text string `json:"text"`
+}
+
+func sendMessage(msg string) (string, error) {
+  zapr.V(5).I("Sending Message...")
+  payload, err := json.Marshal(UserMessage{Sender: "santi", Message: msg})
+  if err != nil {
+    zapr.V(3).E("Failed to encode json string", zap.Any("error", err))
+    return "", err
+  }
+
+  zapr.V(5).I("Sending msg to server", zap.Any("payload", payload))
+  response, err := http.Post("http://localhost:5005/webhooks/rest/webhook", "application/json", bytes.NewBuffer(payload))
+  if err != nil {
+    zapr.V(3).E("Failed to send message", zap.Any("error", err))
+    return "", err
+  }
+
+  defer response.Body.Close()
+  body, err := io.ReadAll(response.Body)
+  if err != nil {
+    zapr.V(3).E("Failed to send message", zap.Any("error", err))
+    return "", err
+  }
+
+  zapr.V(8).I("Somesing", zap.Any("sing", body))
+  var botMsg []BotMessage
+  err = json.Unmarshal(body, &botMsg)
+  if err != nil {
+    zapr.V(3).E("Failed to parse server response", zap.Any("error", err))
+    return "", err
+  }
+
+  for i := 0; i < len(botMsg); i++ {
+    zapr.V(3).I("Received bot messages", zap.Any("text", botMsg[i].Text))
+  }
+  return "haha", nil
+}
+
+func getMessage() (string, error) {
+  zapr.V(5).I("Getting Message...")
+  var msg string
+  n, err := fmt.Scanln(&msg)
+  if err != nil || n <= 0 {
+    return "", err
+  }
+  return msg, nil
+}
+
 func main() {
-  zapr.Init(9)
+  vLevel := flag.Int("v", 3, "Verbosity level.")
+  flag.Parse()
+
+  zapr.Init(uint8(*vLevel))
   defer zapr.Sync()
-  zapr.I("ok")
-  zapr.V(8).I("um ok")
+
+  for {
+    msg, err := getMessage()
+    if err != nil {
+      zapr.V(3).I("Finished", zap.Any("error", err))
+      break
+    }
+    zapr.V(5).I("Got message:", zap.Any("message", msg))
+
+    response, err := sendMessage(msg)
+    if err != nil {
+      zapr.V(1).E("Error:", zap.Any("error", err))
+      break
+    }
+    zapr.V(5).I("Response:", zap.Any("response", response))
+  }
 }
